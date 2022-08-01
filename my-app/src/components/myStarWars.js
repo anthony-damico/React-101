11bfd582c32f0f159147fdaf55c748bc1dd4ea94
Anthony D'Amico <anthony_damico@hotmail.com>
2022-08-01 15:56:10 +1000

    Added some "Stored State"

--- my-app/src/components/myStarWars.js
+++ my-app/src/components/myStarWars.js
@@ -1,18 +1,37 @@
 import React from 'react';
 
 class StarWars extends React.Component {
+    constructor(props) {
+        super(props)
+
+        //Set the state to blank on setup
+        this.state = {
+            name: null,
+            height: null,
+            homeworld: null,
+            films: [],
+        }
+    }
+
+    //Everytime getNewCharacter() is called, we need to update the state
     getNewCharacter() {
         console.log("Get new character from a button")
+        this.setState({
+            name: 'Luke',
+            height: 172,
+            homeworld: 'Tatooine',
+            films: ['item 1', 'item 2'],
+        })
     }
 
     render() {
         return (
         <div>
-            <h1>Name</h1>
-            <p>Height (cm)</p>
-            <p>Homeworld: URL GOES HERE</p>
+            <h1>{this.state.name}</h1>
+            <p>{this.state.height} (cm)</p>
+            <p>Homeworld: {this.state.homeworld}</p>
             <ul>
-                <li>Films: GO HERE</li>
+                <li>{this.state.films}</li>
             </ul>
             <button type="button" onClick={() => this.getNewCharacter()} className="btn">Randomize Character</button>
             </div>
