430fbc67dd467a1a50de33e22610b00ec19ec0e9
Anthony D'Amico <anthony_damico@hotmail.com>
2022-08-01 15:49:28 +1000

    Added onClick event to the myStarWars.js

--- my-app/src/components/myStarWars.js
+++ my-app/src/components/myStarWars.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
 class StarWars extends React.Component {
-  render() {
-    return (
-      <div>
-          <h1>Name</h1>
-          <p>Height (cm)</p>
-          <p>Homeworld: URL GOES HERE</p>
-          <ul>
-              <li>Films: GO HERE</li>
-          </ul>
-          <button type="button" className="btn">Randomize Character</button>
-      </div>
-    )    
-  }
+    getNewCharacter() {
+        console.log("Get new character from a button")
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>Name</h1>
+            <p>Height (cm)</p>
+            <p>Homeworld: URL GOES HERE</p>
+            <ul>
+                <li>Films: GO HERE</li>
+            </ul>
+            <button type="button" onClick={() => this.getNewCharacter()} className="btn">Randomize Character</button>
+            </div>
+        )    
+    }
 }
 
 export default StarWars;
